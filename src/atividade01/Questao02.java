327f573e4c1b151cfb91fab88eb5b52f3d2f83ca
isaquelevi <isaquelevi@gmail.com>
2022-10-16 10:41:58 -0300

    programa questao 02

---
+++ src/atividade01/Questao02.java
@@ -0,0 +1,21 @@
+package atividade01;
+import java.util.Scanner;
+
+public class Questao02 {
+    public static void main(String[] args) throws Exception {
+        // ferramenta que vai permitir utilizar a função de leitura.
+        Scanner ler = new Scanner(System.in);
+        // 1. Escrever a mensagem "Digite um número."
+        System.out.println("Digite o numero base.");
+        // 2. Ler o número
+        int n1 = ler.nextInt();
+        n1 = ++n1;
+        System.out.println("Vizinho a direita do Numero Base: "+n1);
+        n1 = --n1;
+        System.out.println("Vizinho a esquerda do Numero Base: "+n1);
+        ler.close();
+
+        
+
+    }
+}
