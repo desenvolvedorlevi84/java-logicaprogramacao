242839f42a4fbda197cef97f92069f02080efc80
isaquelevi <isaquelevi@gmail.com>
2022-10-19 17:01:18 -0300

    laços de repetição: while; do-while; for

---
+++ src/exemplos/ExemploLacoRepeticao.java
@@ -0,0 +1,25 @@
+package exemplos;
+
+public class ExemploLacoRepeticao {
+    public static void main(String[] args) {
+        int contar = 1; // i
+        while (contar <= 10) {
+            System.out.print(contar);
+            System.out.println(" < dentro do loop while");
+            contar++; // equivalente> contar = contar + 1;
+        }
+
+        do {
+            contar--; // equivalente> contar = contar - 1;
+            System.out.print(contar);
+            System.out.println(" <  dentro do loop do-while");
+        } while (contar > 1);
+
+        // 1-start da variavel; 2-condição da repetição; forma de incremento 
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i);
+            System.out.println(" < dentro de loop for");
+        }
+    }
+    
+}
