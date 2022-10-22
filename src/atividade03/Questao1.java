3425f093863b85e84fd511f03ee2f3040b50c6ab
isaquelevi <isaquelevi@gmail.com>
2022-10-22 12:49:29 -0300

    questao001

--- src/atividade03/Questao1.java
+++ src/atividade03/Questao1.java
@@ -3,16 +3,46 @@ import java.util.Scanner;
 public class Questao1 {
     // 1. Escreva um programa que vai ler um número, o sistema vai escrever na tela a tabuada de 1 até 10 do número digitado.
     public static void main(String[] args) {
-        try (Scanner ler = new Scanner(System.in)) {
+            Scanner ler = new Scanner(System.in);
             System.out.println("Escolha o numero");
-            int num = ler.nextInt();
-            while (num <= 10) {
-                System.out.print(num);
-                System.out.println(" < dentro do loop while");
-                num++;
-                
+            int contar = ler.nextInt();
+            int multi = 0;
+            int div = 0;
+            int soma = 0;
+            int subtracao = 0;
+
+            System.out.println("== Multiplicação ==");
+
+        for (int x = 0; x < 10; x++) {
+            int total = contar * ++multi;
+            System.out.println(contar + " x " + multi + " = " + total);
             }
-        }
+
+            System.out.println("== Divisão ==");
+        
+        for (int x = 0; x < 10; x++) {
+            int total = contar / ++div;
+            System.out.println(contar + " / " + div + " = " + total);
+            }
+
+            System.out.println("== Soma ==");
+
+        for (int x = 0; x < 10; x++) {
+            int total = contar + ++soma;
+            System.out.println(contar + " + " + soma + " = " + total);
+            } 
+
+            System.out.println("== Subtracação ==");
+
+        for (int x = 0; x < 10; x++) {
+            int total = contar - ++subtracao;
+            System.out.println(contar + " - " + subtracao + " = " + total);
+            }
+
+
+
+         ler.close();
+        
       
     }
     
