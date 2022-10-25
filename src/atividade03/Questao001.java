8e4c71a86d729927df8e85ce92abdb469fd1681f
isaquelevi <isaquelevi@gmail.com>
2022-10-25 16:01:00 -0300

    correcao final questao001

--- src/atividade03/Questao001.java
+++ src/atividade03/Questao001.java
@@ -1,49 +1,21 @@
 package atividade03;
+
 import java.util.Scanner;
+
 public class Questao001 {
-    // 1. Escreva um programa que vai ler um número, o sistema vai escrever na tela a tabuada de 1 até 10 do número digitado.
+    /**
+     * 1. Escreva um programa que vai ler um número,
+     * o sistema vai escrever na tela a tabuada de 1 até 10 do número digitado.
+     */
     public static void main(String[] args) {
-            Scanner ler = new Scanner(System.in);
-            System.out.println("Escolha o numero");
-            int contar = ler.nextInt();
-            int multi = 0;
-            int div = 0;
-            int soma = 0;
-            int subtracao = 0;
-
-            System.out.println("== Multiplicação ==");
-
-        for (int x = 0; x < 10; x++) {
-            int total = contar * ++multi;
-            System.out.println(contar + " x " + multi + " = " + total);
-            }
-
-            System.out.println("== Divisão ==");
-        
-        for (int x = 0; x < 10; x++) {
-            int total = contar / ++div;
-            System.out.println(contar + " / " + div + " = " + total);
-            }
-
-            System.out.println("== Soma ==");
-
-        for (int x = 0; x < 10; x++) {
-            int total = contar + ++soma;
-            System.out.println(contar + " + " + soma + " = " + total);
-            } 
-
-            System.out.println("== Subtracação ==");
-
-        for (int x = 0; x < 10; x++) {
-            int total = contar - ++subtracao;
-            System.out.println(contar + " - " + subtracao + " = " + total);
-            }
-
-
-
-         ler.close();
-        
-      
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um número.");
+        int numero = ler.nextInt();
+        // while, do-while, for esta correto
+        for (int i = 1; i <= 10; i++) {
+            // System.out.println(String.format("%s x %s = %s ", numero, i, i * numero));
+            System.out.printf("%s x %s = %s \n", numero, i, i * numero);
+        }
+        ler.close();
     }
-    
 }
