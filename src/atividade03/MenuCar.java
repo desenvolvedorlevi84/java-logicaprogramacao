a3e1e99be6195031997fba4ae1f17a920dbe1417
isaquelevi <isaquelevi@gmail.com>
2022-11-01 22:00:09 -0300

    atividade 01112022

---
+++ src/atividade03/MenuCar.java
@@ -0,0 +1,14 @@
+package atividade03;
+
+public class MenuCar {
+    public static void main(String[] args) throws Exception {
+        Carro c1 = new Carro("FIAT","Palio");
+        c1.printStatus();
+        c1.acelerar(20);
+        c1.acelerar(50);
+        c1.acelerar(200);
+        c1.frear(100);
+        c1.frear(150);
+        c1.printStatus();
+}
+}
